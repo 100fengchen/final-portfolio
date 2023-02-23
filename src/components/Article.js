b169777607920712fc8b90c0518322a5b65d1e1c
100fengchen <124771585+100fengchen@users.noreply.github.com>
2023-02-23 21:35:41 +0200

    final

--- src/components/Article.js
+++ src/components/Article.js
@@ -12,10 +12,8 @@ export default function Article() {
                     I find that garments have this element of being timeless in terms of design and fabric, and there is a desire to be able to wear and use the garment for a lifetime. I do not believe in the concept of a perfect garment, only in the knowledge that you can be absolutely hideous and still look good. I also believe at the end of the day that it’s about the satisfaction of being able to wear something that you love, and not the piece that you wore for eight years.
                     </h1>
                 </article>
-                <div id="rectangle">
-                    <button class="BigName">Pai-Feng, 
-                        Chen</button>
-                </div> 
+                
+                <div id="rectangle"><button class="BigName">Pai-Feng, Chen</button></div>
             </div>
             <div id = "potraitbox">
             <img class="Portrait" src={Portrait} alt="My handsome selfie hehe"/>
