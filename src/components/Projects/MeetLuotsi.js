ccc911f48ef3455dad6619ba8ea4b790145204bc
100fengchen <124771585+100fengchen@users.noreply.github.com>
2024-09-04 17:21:42 +0300

    common change

--- src/components/Projects/MeetLuotsi.js
+++ src/components/Projects/MeetLuotsi.js
@@ -4,7 +4,7 @@ export default function MeetLuotsi() {
   return (
     <div className='ProductFullPage'>
       <div className='pageBox'>
-        <img src={cover} className='MLcover'></img>
+        <img src={cover} className='SpotOnCover'></img>
         <h1 className="description"> {/* Fixed typo: Changed 'desicription' to 'description' */}
         Senioripaku (translating to ‘Senior Van’) lunched in 2022 organises activities for senior citizens at multiple locations in Espoo, offering guidance, physical exercise and arts and culture talks. As part of the Designing for Services course 2022 in Aalto university and in collaboration with the City of Espoo, we aimed to promote the serivce with participatory approaches.
         </h1>
